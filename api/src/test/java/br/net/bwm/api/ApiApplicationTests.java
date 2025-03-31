327994239d110817d3b0faec68b69e2364d39b60
Tarcísio Alves <tarcisio.bwm@gmail.com>
2025-03-31 15:59:03 -0300

    Iniciado o projeto

---
+++ api/src/test/java/br/net/bwm/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.bwm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
